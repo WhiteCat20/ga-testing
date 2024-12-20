677919092fd29ffed7d13e13edc81ebb3a0ece9c
Faiz Rahmadani <fzrahmadan@gmail.com>
2024-12-20 16:49:42 +0700

    ci

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.get("/", (req, res) => res.send("This App is running properly!"));
 app.get("/ping", (req, res) => res.send("Pong!"));
 app.get("/youtube", (req, res) => res.send("Hello, youtube indonesia!"));
 app.get("/coding", (req, res) => res.send("Hello, coding!"));
+app.get("/ci", (req, res) => res.send("Hello, ci!"));
 app.get("/hello/:name", (req, res) => {
   res.json({ message: greeting(req.params.name) });
 });
